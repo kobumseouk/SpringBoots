d224b3a9c690093fa09538256ccc2fc1fe63fc71
이찬진 <chanjin23@naver.com>
2024-11-05 18:20:55 +0900

    fix : 리프레시토큰 쿠키 재발급 -> redis DB 토큰 재발급 수정

--- src/main/java/com/spring_boots/spring_boots/config/jwt/JwtFilter.java
+++ src/main/java/com/spring_boots/spring_boots/config/jwt/JwtFilter.java
@@ -1,5 +1,6 @@
 package com.spring_boots.spring_boots.config.jwt;
 
+import com.spring_boots.spring_boots.common.util.CookieUtil;
 import com.spring_boots.spring_boots.config.jwt.impl.JwtProviderImpl;
 import jakarta.servlet.*;
 import jakarta.servlet.http.Cookie;
@@ -16,8 +17,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Optional;
 
-import static com.spring_boots.spring_boots.config.jwt.UserConstants.ACCESS_TOKEN_TYPE_VALUE;
-import static com.spring_boots.spring_boots.config.jwt.UserConstants.REFRESH_TOKEN_TYPE_VALUE;
+import static com.spring_boots.spring_boots.config.jwt.UserConstants.*;
 
 @RequiredArgsConstructor
 @Component
@@ -32,35 +32,45 @@ public class JwtFilter extends OncePerRequestFilter {
                                     FilterChain filterChain) throws ServletException, IOException {
         // JWT 토큰을 쿠키에서 추출
         String jwtAccessToken = resolveAccessTokenFromCookies(request);
-        String jwtRefreshToken = resolveRefreshTokenFromCookies(request);
 
+        //로그인 api 거나 액세스토큰을 가지고있지 않다면 바로 통과
         String requestURI = request.getRequestURI();
-        if (requestURI.startsWith("/api/login")) {
+        if (requestURI.startsWith("/api/login") || jwtAccessToken==null) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        if (jwtAccessToken != null && tokenProvider.validateToken(jwtAccessToken)) {
+        boolean validateToken = tokenProvider.validateToken(jwtAccessToken);
+        if (validateToken) {
             // 액세스토큰이 유효한 경우, Authentication 객체 생성
             Authentication authentication = tokenProvider.getAuthentication(jwtAccessToken);
             SecurityContextHolder.getContext().setAuthentication(authentication);
-        } else if (jwtRefreshToken != null) {
+        } else {
             // 액세스토큰이 만료된 경우, 리프레시토큰 검증
             log.info("Access token expired, validating refresh token...");
 
             // 리프레시토큰이 유효하다면 새로운 액세스토큰 발급
             // 해당 메소드에서 유효성 검사까지 같이 진행
-            String newAccessToken = tokenProvider.generateAccessTokenFromRefreshToken(jwtRefreshToken);
+//            String newAccessToken = tokenProvider.generateAccessTokenFromRefreshToken(jwtRefreshToken);
+
+            //레디스에 있는 리프레시토큰을 가져와 새로운 엑세스토큰 발급
+            String newAccessToken = tokenProvider.generateAccessTokenFromRefreshTokenByRedis(jwtAccessToken);
 
             log.info("리프레시토큰 발급 완료..!");
 
+
+            int cookieMaxAge = (int) ACCESS_TOKEN_DURATION.toSeconds(); // 쿠키 유효 기간 설정
+
+            //쿠키 생성
+            CookieUtil.addCookie(response,ACCESS_TOKEN_TYPE_VALUE,newAccessToken,cookieMaxAge);
+
             // 새로운 액세스토큰을 쿠키에 저장
-            Cookie newAccessTokenCookie = new Cookie(ACCESS_TOKEN_TYPE_VALUE, newAccessToken);
-            newAccessTokenCookie.setHttpOnly(true);
-            newAccessTokenCookie.setSecure(true);
-            newAccessTokenCookie.setAttribute("SameSite", "Lax");
-            newAccessTokenCookie.setPath("/");
-            response.addCookie(newAccessTokenCookie);
+//            Cookie newAccessTokenCookie = new Cookie(ACCESS_TOKEN_TYPE_VALUE, newAccessToken);
+//            newAccessTokenCookie.setHttpOnly(true);
+//            newAccessTokenCookie.setSecure(true);
+//            newAccessTokenCookie.setAttribute("SameSite", "Lax");
+//            newAccessTokenCookie.setPath("/");
+//            response.addCookie(newAccessTokenCookie);
 
             // 새로운 액세스토큰으로 Authentication 객체 생성
             Authentication authentication = tokenProvider.getAuthentication(newAccessToken);
