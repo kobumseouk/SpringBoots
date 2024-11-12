6c7632181d18239ea3e81e7fe69017e846b4ffd8
[CLOUD4] 고범석 <kbssky94@gmail.com>
2024-11-12 19:27:15 +0900

    fix: LocalDate Jackson 형식으로 직렬화/역직렬화 의존성 추가

--- src/main/java/com/spring_boots/spring_boots/config/redis/SearchLogRedisConfig.java
+++ src/main/java/com/spring_boots/spring_boots/config/redis/SearchLogRedisConfig.java
@@ -1,6 +1,11 @@
 package com.spring_boots.spring_boots.config.redis;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.spring_boots.spring_boots.item.entity.SearchHistory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -8,17 +13,33 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Slf4j
+@EnableCaching
 @Configuration
+@RequiredArgsConstructor
 public class SearchLogRedisConfig {
+
   @Bean
   public RedisTemplate<String, SearchHistory> searchLogRedisTemplate(RedisConnectionFactory connectionFactory) {
-    RedisTemplate<String, SearchHistory> template = new RedisTemplate<>();
-    template.setConnectionFactory(connectionFactory);
+    RedisTemplate<String, SearchHistory> searchTemplate = new RedisTemplate<>();
+    searchTemplate.setConnectionFactory(connectionFactory);
+
+    // ObjectMapper 설정
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
 
     // JSON 직렬화를 위한 설정
-    template.setKeySerializer(new StringRedisSerializer());
-    template.setValueSerializer(new Jackson2JsonRedisSerializer<>(SearchHistory.class));
+    Jackson2JsonRedisSerializer<SearchHistory> jsonSerializer =
+        new Jackson2JsonRedisSerializer<>(SearchHistory.class);
+    jsonSerializer.setObjectMapper(objectMapper);
 
-    return template;
+    searchTemplate.setKeySerializer(new StringRedisSerializer());
+    searchTemplate.setValueSerializer(jsonSerializer);
+    searchTemplate.setHashKeySerializer(new StringRedisSerializer());
+    searchTemplate.setHashValueSerializer(jsonSerializer);
+
+    return searchTemplate;
   }
+
+
 }
